1325ec7cfb5f556eb3c9094258045b56cbcab8bb
James Bos <jamesbos@gmail.com>
2017-01-30 14:30:16 +0700

    Housecleaning...

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 #MPD2MD
 
-Push currently playing MPD track to remote text file.
+Push currently playing MPD track to a local or remote text file.
+
+## Options
+
+If no need to push via SCP, feel free to comment out self.push2webscp(). 
+
+Only options that required to be set
+    self.server = "Your SSH host"
+    self.remotepath = "/path/to/file" # Where you want the generated file saved.
+
+Note: When copying to remote server over ssh/scp, ensure you've set up ssh-copy-id to allow non-interactive ssh (ie: Key based auth, not password based)
 
 ## Use cases
 
-Assign to a keyboard shortcut to ensure you remember currently playing song. Especially suited to programmers with concentration spans easily broken.
+Assign to a keyboard shortcut (xbindkeys?) to ensure you remember currently playing song. Especially suited to programmers with concentration spans easily broken.
+
+Example key binding in your ~/.xbindkeysrc:
+    "/path/to/mpd2md.py"
+        Control + mod4 + s
 
 ## Python Requirements
 
-* paramiko (pip install paramiko)
-* mpd (pip install python-mpd2)
+* paramiko
+* mpd
 
--- mpd2web.py
+++ mpd2web.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 from mpd import MPDClient
 import paramiko
 import os
@@ -10,14 +10,9 @@ class MPD2Web:
 		self.client.timeout = 10
 		self.client.idletimeout = None
 		self.client.connect("localhost",6600)
-		self.filename = 'out.txt'
-		self.server = 'jamesbos.com'
-		# self.remotepath = '/var/www/somehtml/staticpages/likedtunes.md'
-		self.remotepath = '/home/james/likedtunes.md'
-
-	# Call this to test if mpd is playing nice
-	def mpd_version(self):
-		return self.client.mpd_version
+		self.filename = 'outfile.txt'
+		self.server = 'localhost' # SSH host, requires password-less/key based auth
+		self.remotepath = '/tmp/likedsongs.md' # Change this to location on remote server.
 
 	def current_song(self):
 		current_song = self.client.currentsong()
@@ -27,17 +22,35 @@ class MPD2Web:
 		name = current_song['name']
 		return "%s" % (title)
 
+	# Check to make sure we haven't accidentally liked the same song twice
+	# (This is only checking the last entry)
+	def check_last_entry(self,current):
+	    with open(self.filename, 'r') as f:
+	        f.seek(-2,2)
+	        while f.read(1) != b"\n":
+	            f.seek(-2,1)
+	        last = f.readline()[:-1]
+	        f.close()
+		if(current == last):
+			return False
+		else:
+			return True
+
 	def push2md(self):
 		now = datetime.now()
 		time = str(datetime.strftime(now,"%Y-%m-%d"))
-		currentsong = self.current_song()
-		outfile = open(self.filename,'a')
-		outfile.write("    "+time+" "+currentsong+"\n")
-		outfile.close()
+		currentsong = "    "+time+" "+self.current_song()
+		if(self.check_last_entry(currentsong)):
+			with open(self.filename, 'a') as out:
+				out.write(currentsong+"\n")
+			out.close()
+			print("{} added.").format(currentsong)
+		else:
+			print("Song already added to last entry")
 
 	def push2webscp(self):
 		ssh = paramiko.SSHClient()
-		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		ssh.load_system_host_keys()
 		ssh.connect(self.server, username='james')
 		sftp = ssh.open_sftp()
